5ba8f9a9cb3019ff5a9e6318a0567893161ec677
Nathan Ratliff <ratliff.nathan@gmail.com>
2016-02-22 00:54:49 -0800

    Modified default config in json riemo descriptor to match new primary workspace axis.
    - x-axis now points forward into the workspace. Used to be the y-axis.
      So the center of the first joint's range (zero) points forward along
      the x-axis. Default config for that joint is now zero to reflect
      neutral as just pointing forward.

--- scripts/run_riemo_robot_emulator.sh
+++ scripts/run_riemo_robot_emulator.sh
@@ -2,11 +2,13 @@
 
 # Executes RieMO robot emulator 
 
-Q_INIT_CSV="0.790,0.054,-1.180,-1.012,1.755,-0.121,0.630,0.607"
+Q_INIT_CSV="-0.614159,0,-0.400798,1.83805,-1.63068,1.42643,0.623563,0.623563"
 
 JOINT_STATE_TOPIC=joint_states
 
 rosrun riemo_programs riemo_robot_emulator \
+  --robot_package_name=nw_riemo_robots \
+  --riemo_json_config=riemo_robot_mico.json \
   --q_init_csv=$Q_INIT_CSV \
   --joint_state_topic=$JOINT_STATE_TOPIC \
 
